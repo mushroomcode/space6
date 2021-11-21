befcfbe109c3b7871c478f98ddbbf21992a658aa
yzn2015 <20156660@s.hlju.edu.cn>
2021-11-21 21:57:20 +0800

    @committer 事务实现

---
+++ src/main/java/com/SpringBootMyersStart.java
@@ -0,0 +1,10 @@
+package com;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMyersStart {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMyersStart.class, args);
+    }
+}
