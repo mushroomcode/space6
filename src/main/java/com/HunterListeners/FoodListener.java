cda0185442cc735305afa2cac4e55e1d7b8d688b
yzn2015 <20156660@s.hlju.edu.cn>
2021-11-29 23:07:48 +0800

    @committer @Author--xiaoguanyue Listener

---
+++ src/main/java/com/HunterListeners/FoodListener.java
@@ -0,0 +1,30 @@
+package com.HunterListeners;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+import javax.servlet.ServletRequestEvent;
+import javax.servlet.ServletRequestListener;
+import javax.servlet.annotation.WebListener;
+
+/**
+ *  Request 监听者初始化，监听Request的创建和销毁
+ */
+@WebListener
+public class FoodListener implements ServletRequestListener {
+
+    private static Logger logger = LoggerFactory.getLogger(FoodListener.class);
+
+    @Override
+    public void requestDestroyed(ServletRequestEvent servletRequestEvent) {
+        System.out.println("============= 监听者被摧毁 ===============");
+//        logger.info("============= 监听者被摧毁 ===============");
+    }
+
+    @Override
+    public void requestInitialized(ServletRequestEvent servletRequestEvent) {
+        System.out.println("============= 监听者 初始化 ===============");
+//        logger.info("============= 监听者 初始化 ===============");
+    }
+}
