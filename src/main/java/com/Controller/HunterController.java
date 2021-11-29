fd9ff2b6f65c83fe2596c571316f7a424954308d
yzn2015 <20156660@s.hlju.edu.cn>
2021-11-29 23:22:24 +0800

    @committer @Author--xiaoguanyue Controller 的进行查看

--- src/main/java/com/Controller/HunterController.java
+++ src/main/java/com/Controller/HunterController.java
@@ -1,10 +1,19 @@
 package com.Controller;
 
+import com.DiffBean.HunterCarParam;
+import com.Servicers.HunterFoodServicer;
 import com.transactions.HunterTrans;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
 
 @Controller
 @RequestMapping("/index")
@@ -12,6 +21,9 @@ public class HunterController {
 
     @Autowired
     private HunterTrans hunterTrans;
+
+    @Autowired
+    private HunterFoodServicer servicer;
 
     //
     @RequestMapping("/hello")
@@ -26,12 +38,33 @@ public class HunterController {
 
     @RequestMapping("/hello2")
     @ResponseBody
-    public String Hello2() {
+    public String Hello2(HttpServletRequest servletRequest) {
+        Enumeration<String> headerNames = servletRequest.getHeaderNames();
         long start = System.currentTimeMillis();
         hunterTrans.asyncTask("async success");
         long end = System.currentTimeMillis();
         System.out.println(end - start);
         return "sleep";
+    }
+
+    @RequestMapping("/hunterCarsList")
+    @ResponseBody
+    public List<HunterCarParam> HunterTreeNode() {
+        List<HunterCarParam> retList = new ArrayList<>();
+        HunterCarParam hunterCarParam = new HunterCarParam();
+        hunterCarParam.setCarBranch("Honda");
+        hunterCarParam.setCarName("civic 11th");
+        hunterCarParam.setCarType("Sedan");
+        retList.add(hunterCarParam);
+        return retList;
+    }
+
+    @GetMapping(value = "/hunterFood")
+    @ResponseBody
+    public String hunterFood(HttpServletRequest request, HttpServletResponse response, @RequestParam("foodName") String foodName,
+                             @RequestParam("energy") String energy) {
+        servicer.addFood(foodName, energy);
+        return "获取:" + foodName;
     }
 
 }
